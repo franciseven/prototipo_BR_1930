f67eaa9e9291c0ed0e388771ffea4f18abf82bd1
franciseven <franciscomarcolino13@gmail.com>
2025-09-17 09:53:24 -0300

    Criação de mecânica de busca avançada a partir de botão na tela inicial

--- home.js
+++ home.js
@@ -89,3 +89,14 @@ heroImages.forEach(img => {
     tooltip.style.opacity = '0';
   });
 });
+
+document.getElementById("toggleFiltrosBtn").addEventListener("click", function() {
+  const filtros = document.getElementById("filtrosAvancados");
+  if (filtros.style.display === "none" || filtros.style.display === "") {
+    filtros.style.display = "block";
+    this.textContent = "－ Busca Avançada";
+  } else {
+    filtros.style.display = "none";
+    this.textContent = "＋ Busca Avançada";
+  }
+});
