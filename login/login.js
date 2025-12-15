8891ff8676964bc8f864c0e4723a277de6a5bb26
franciseven <franciscomarcolino13@gmail.com>
2025-12-15 14:29:06 -0300

    correção de erro para as credenciais de login

--- login/login.js
+++ login/login.js
@@ -1,6 +1,24 @@
 document.getElementById('login-form').addEventListener('submit', function (e) {
   e.preventDefault();
 
+  const senhaInput = document.getElementById('senha');
+  const senha = senhaInput.value;
+
+  function senhaForte(senha) {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&\-_#])[A-Za-z\d@$!%*?&\-_#]{8,}$/;
+    return regex.test(senha);
+  }
+
+  if (!senhaForte(senha)) {
+    senhaInput.setCustomValidity(
+      "A senha deve ter no mínimo 8 caracteres, incluindo letra maiúscula, minúscula, número e caractere especial."
+    );
+    senhaInput.reportValidity();
+    return;
+  } else {
+    senhaInput.setCustomValidity("");
+  }
+
   if (this.checkValidity()) {
     window.location.href = "../perfil/administrador.html";
   } else {
